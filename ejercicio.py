1a0f762fa7cc21761671faed1cea2acd982ea58c
miguelpz <miguelpz@gmail.com>
2018-09-08 12:58:26 +0200

    primer commit

---
+++ ejercicio.py
@@ -0,0 +1,18 @@
+# Inicializar lista
+lista=[]
+
+# Recorrer números del 1 al 100
+
+for n in range(1,101):
+    
+    # Condición
+    if ((n%3)==0 and (n%4)==0 ):
+            lista.append(n)
+
+print (lista)
+            
+
+    
+    # Añadir elemento a la lista
+    
+
---
+++ listas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Definir Variables
+lista=[1,2,3,4,5]
+tupla=("uno","dos","tres","cuatro","cinco")
+#diccionario={1:"uno",2:"dos",3:"tres",4:"cuatro",5:"cinco"}
+print (dict(zip(lista,tupla)))
+#print diccionario (zip(tupla,lista))
+
+
+# Visualizar claves
+# print(diccionario.keys())
+# Visualizar valores
+# print(diccionario.values())
