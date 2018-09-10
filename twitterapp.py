80f5fc5b552eac4dd73e7a5246471b81d510c9d7
miguelpz <miguelpz@gmail.com>
2018-09-10 09:13:46 +0200

    Uso del mudulo

--- twitterapp.py
+++ twitterapp.py
@@ -1,3 +1,5 @@
+from modtwitter import *
+
 creditos = ("Miguel Angel","sutia",2018)
 infotweet={}
 listatweet=[]
@@ -6,35 +8,11 @@ usuario = input("Cuel es tu nombre?")
 otro="s"
 i=1
 
-def pedirtweet():
-    tweet = None
-    tweet = input ("Que esta pasando? ")
-    likes = int(input("Cuantos likes? "))
-    retweets = int(input("Cuantos retweets? "))
-    infotweet ={"Autor":usuario, "Mensaje":tweet, "Likes":likes, "Retweets":retweets}
-    return infotweet
-
-def flistatweets (listatweet):
-    for tweet in listatweet:
-        print(str(tweet))
-        likes = tweet['Likes']
-        retweets = tweet['Retweets']
-
-        if likes > 2:
-            print("Su tweet tiene 'me gustas'")
-        elif likes == 1:
-            print("Su tweet tiene un me gusta")
-        elif likes == 2:
-            print("Su tweet tiene dos me gusta")
-        else:
-            print("Su tweet no tiene 'me gustas'")
-        
-        if (retweets > 0): print("Este tweet tiene retweets")
     
 while (otro == "s"):
     print ("Tweet numero: ", i)
     i += 1
-    infotweet = pedirtweet()
+    infotweet = pedirtweet(usuario)
     listatweet.append(infotweet)
     otro = input("Desea introducir un nuevo tweet? s/n: ")
     flistatweets (listatweet)
